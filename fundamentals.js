6984315d02d2a02b23d345c1cfc9ccb142b15981
Mary Akastia Christo <n.akastia@gmail.com>
2022-09-16 12:47:07 +0200

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -5,11 +5,11 @@ const aCar = {
         address : ['1 Main Street', '2 High Street']
     },
      
-
     type : {
         make : 'Toyota', 
         model : 'Corolla', 
         cc: 1.8},
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : {
         year: 201,
         county : 'WD', 
@@ -25,9 +25,23 @@ const aCar = {
   };
   
 
-
+// console logs
   console.log(aCar.owner);
   console.log('First owner : ' + aCar.pervious_owners.name[0] + ' - ' + aCar.pervious_owners.address[0])
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
   console.log('It is a ' + aCar.colour.exterior_colour + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior_fabric.texture + ' interior..')
+// Shows the features of the car
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+
+// Displays the names of the previous owner
+for (let i = 0 ; i < aCar.pervious_owners.name.length ; i += 1) {
+    console.log(aCar.pervious_owners.name[i]);
+}
+
+// shows the type of car
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
